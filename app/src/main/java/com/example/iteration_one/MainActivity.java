5468ee750dd01db65cdf6bf0d429d9817118f1fc
Samuel Jennings <sjen0011@student.monash.edu.au>
2022-08-29 22:03:37 +1000

    Initial commit
    First log-in screen complete
    id elements declared in MainActivity.java

--- app/src/main/java/com/example/iteration_one/MainActivity.java
+++ app/src/main/java/com/example/iteration_one/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -13,9 +14,9 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText email, password;
     private RelativeLayout anonLogIn, gmailLogin, facebookLogin;
-    private LinearLayout signIn;
+    private LinearLayout signIn, kidOne, kidTwo;
     private TextView forgotPass, signUp;
-
+    private ImageView addPic;
 
 
 
@@ -32,7 +33,9 @@ public class MainActivity extends AppCompatActivity {
         gmailLogin = findViewById(R.id.rl_gmail_login);
         facebookLogin = findViewById(R.id.rl_facebook_login);
         signUp = findViewById(R.id.tv_sign_up);
-
+        addPic = findViewById(R.id.iv_camera);
+        kidOne = findViewById(R.id.ll_kid_one);
+        kidTwo = findViewById(R.id.ll_kid_two);
 
     }
 }
