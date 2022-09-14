82c0b66c82732a8f0b3870807a903b163b19e4ff
Samuel Jennings <sjen0011@student.monash.edu>
2022-09-15 09:20:12 +1000

    Completed: Add Food Diary completed Food elimination blank page done (Java ids not yet declared) food elimination item WIP (Java ids not yet declared)

--- app/src/main/java/com/example/iteration_two/AddFoodDiaryActivity.java
+++ app/src/main/java/com/example/iteration_two/AddFoodDiaryActivity.java
@@ -3,12 +3,30 @@ package com.example.iteration_two;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class AddFoodDiaryActivity extends AppCompatActivity {
+
+    private RelativeLayout cancelAddMeal, selectBreakfast, selectLunch, selectDinner, selectSnack, confirmAddMeal;
+    private EditText mealTitle, mealDescription;
+    private LinearLayout addSymptoms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_food_diary);
+
+        cancelAddMeal = findViewById(R.id.rl_cancel_add_meal);
+        selectBreakfast = findViewById(R.id.rl_select_breakfast);
+        selectLunch = findViewById(R.id.rl_select_lunch);
+        selectDinner = findViewById(R.id.rl_select_dinner);
+        selectSnack = findViewById(R.id.rl_select_snack);
+        mealTitle = findViewById(R.id.et_meal_title);
+        mealDescription = findViewById(R.id.et_meal_description);
+        addSymptoms = findViewById(R.id.ll_add_symptoms);
+        confirmAddMeal = findViewById(R.id.rl_add_meal_confirm);
+
     }
 }
