8ee4dbb39e1aad649b188355863599302016ae2d
Samuel Jennings <sjen0011@student.monash.edu>
2022-09-18 19:21:10 +1000

    Created Food Intolerance text strings Created lists of strings for all food intolerance symptoms Created lists of strings for all food intolerance common foods
    Updated 'Lethargic' to 'Tiredness'
    Updated 'Respiratory Problems' to 'Trouble Breathing'
    Removed 'Joint Pains' from symptoms
    Added 'Gas' to symptoms

--- app/src/main/java/com/example/iteration_two/FoodIntoleranceInformationActivity.java
+++ app/src/main/java/com/example/iteration_two/FoodIntoleranceInformationActivity.java
@@ -6,9 +6,32 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class FoodIntoleranceInformationActivity extends AppCompatActivity {
 
     private TextView foodIntoleranceItem, foodIntoleranceText, symptomOne, symptomTwo, symptomThree, symptomFour, symptomFive, symptomSix, foodOne, foodTwo, foodThree, foodFour, foodFive;
+//    private Arraylist dairySymptomsList, dairyFoodsList,glutenSymptomsList, glutenFoodsList, soySymptomsList, soyFoodsList, eggsSymptomsList, eggsFoodList, fructoseSymptomsList, fructoseFoodsList, fodmapsSymptomsList, fodmapsFoodsList, salicylatesSymptomsList, salicylatesFoodsList, histamineSymptomsList, histamineFoodsList;
+    private String dairyText = "@string/string_dairy_info", glutenText = "@string/string_gluten_info", soyText = "@string/string_soy_info", eggsText = "@string/string_eggs_info", fructoseText = "@string/string_fructose_info", fodmapsText = "@string/string_fodmaps_info", salicylatesText = "@string/string_salicylates_info", histamineText = "@string/string_histamine_info",
+    /*symptoms*/
+            dairySymptomsList[] = new String[]{"Abdominal Pain", "Bloating", "Diarrhoea", "Gas", "Nausea", "Indigestion"},
+            glutenSymptomsList[] = new String[]{"Abdominal Pain", "Bloating", "Diarrhoea", "Headaches", "Nausea", "Eczema"},
+            soySymptomsList[] = new String[]{"Abdominal Pain", "Bloating", "Diarrhoea", "Gas", "Nausea", "Fatigue"},
+            eggsSymptomsList[] = new String[]{"Abdominal Pain", "Bloating", "Diarrhoea", "Gas", "Nausea", "Indigestion"},
+            fructoseSymptomsList[] = new String[]{"Abdominal Pain", "Bloating", "Diarrhoea", "Gas", "Nausea", "Vomiting"},
+            fodmapsSymptomsList[] = new String[]{"Abdominal Pain", "Bloating", "Diarrhoea", "Gas", "Fatigue", "Eczema"},
+            salicylatesSymptomsList[] = new String[]{"Eczema", "Trouble Breathing", "Diarrhoea", "Abdominal Pain", "Gas", "Hives"},
+            histamineSymptomsList[] = new String[]{"Abdominal Pain", "Eczema", "Headaches", "Digestive Issues", "Trouble Breathing", "Irregular Bodily Functions"},
+    /*foods*/
+            dairyFoodsList[] = new String[]{"Milk", "Ice Cream", "Yoghurts", "Fresh Cheeses", "Creams"},
+            glutenFoodsList[] = new String[]{"Bread", "Baked Goods", "Pasta", "Cereals", "Crackers"},
+            soyFoodsList[] = new String[]{"Soy Sauce", "Tofu", "Soy-based Products", "Asian-style Sauces", "Vegan-safe Products"},
+            eggsFoodsList[] = new String[]{"Eggs", "Custards", "Mayonnaise", "Meringue", "Quiches"},
+            fructoseFoodsList[] = new String[]{"All Fruits", "Honey", "Spinach", "Sweet Sauces", "Sugary Cereals"},
+            fodmapsFoodsList[] = new String[]{"Bread", "Milk-based Products", "Some Fruits", "Honey", "Cakes"},
+            salicylatesFoodsList[] = new String[]{"Spices", "Some Fruits", "Onion", "Honey", "Caffeinated Drinks"},
+            histamineFoodsList[] = new String[]{"Cheeses", "Dairy Products", "Chocolate", "Nuts", "Caffeinated Drinks"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +51,7 @@ public class FoodIntoleranceInformationActivity extends AppCompatActivity {
         foodThree = findViewById(R.id.tv_common_foods_three);
         foodFour = findViewById(R.id.tv_common_foods_four);
         foodFive = findViewById(R.id.tv_common_foods_five);
+
 
     }
 }
