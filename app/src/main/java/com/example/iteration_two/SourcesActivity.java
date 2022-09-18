6b8790e3b14b9bde91e7c920e0d57d8ae34a2bff
Samuel Jennings <sjen0011@student.monash.edu>
2022-09-18 15:14:43 +1000

    Completed: Sources Page XML
    No JAVA ids required

---
+++ app/src/main/java/com/example/iteration_two/SourcesActivity.java
@@ -0,0 +1,14 @@
+package com.example.iteration_two;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SourcesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sources);
+    }
+}
